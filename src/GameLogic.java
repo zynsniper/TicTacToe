9c046f6c2a6bc6a064f15d661da21177145c26d1
zynsniper <zynsniper41@gmail.com>
2025-05-01 14:14:53 -0300

    chosenTile() updated to detemine tile coordinate of dynamic mapSize

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -75,30 +75,11 @@ public class GameLogic {
         return ' ';
     }
 
-    public static int[] chosenTile(int tileNo){
-        switch (tileNo) {
-            case 1:
-                return new int[]{0,0};
-            case 2:
-                return new int[]{0,2};
-            case 3:
-                return new int[]{0,4};
-            case 4:
-                return new int[]{2,0};
-            case 5:
-                return new int[]{2,2};
-            case 6:
-                return new int[]{2,4};
-            case 7:
-                return new int[]{4,0};
-            case 8:
-                return new int[]{4,2};
-            case 9:
-                return new int[]{4,4};
-            
-            default:
-                return new int[]{-1,-1};
-        }
+    public static int[] chosenTile(int tileNo, int playableTilesPerRow){
+        int row = (tileNo - 1) / playableTilesPerRow;
+        int col = (tileNo - 1) % playableTilesPerRow;
+
+        return new int[]{row * 2, col * 2};
     }
 
     public static void printGameBoard(char[][] gameBoard){
