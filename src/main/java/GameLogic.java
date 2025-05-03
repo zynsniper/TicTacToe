768ff575649957326a0a409505b7a9004c304606
zynsniper <zynsniper41@gmail.com>
2025-05-03 14:37:32 -0300

    eevert "changed checkLine() -> connected(); optimised gameEndFX() for readability"
    This reverts commit a63528754d1b3b3df1c861940c266c6685a99c2f.

--- src/main/java/GameLogic.java
+++ src/main/java/GameLogic.java
@@ -1,47 +1,38 @@
 public class GameLogic {
     public static boolean gameEndFX(char[][] board) {
-        //rows and columns
-        for(int i = 0; i < board.length; i++){
-            //row
-            if(connected(board[i][0], board[i])) return true;
+        int N = board.length;
 
-            //column
-            char[] col = new char[board.length];
-            for (int j = 0; j < board.length; j++){
-                col[j] = board[j][i];
-                if (connected(board[0][i], col)) return true;
-            }
+        // Rows and Columns
+        for (int i = 0; i < N; i++) {
+            if (checkLine(board[i][0], board[i])) return true;
+            char[] col = new char[N];
+            for (int j = 0; j < N; j++) col[j] = board[j][i];
+            if (checkLine(board[0][i], col)) return true;
         }
 
-        //diagonals
-        char[] diag1 = new char[board.length];
-        char[] diag2 = new char[board.length];
-        for(int i = 0; i < board.length; i++){
-            //backward
+        // Diagonals
+        char[] diag1 = new char[N];
+        char[] diag2 = new char[N];
+        for (int i = 0; i < N; i++) {
             diag1[i] = board[i][i];
-            //forward
-            diag2[i] = board[i][board.length - 1 - i];
+            diag2[i] = board[i][N - 1 - i];
         }
-        return connected(board[0][0], diag1) || connected(board[0][board.length - 1], diag2);
+        return checkLine(board[0][0], diag1) || checkLine(board[0][N - 1], diag2);
     }
 
-    private static boolean connected(char player, char[] line){
-        boolean answer = true;
-        if(player == ' ') answer = false;
-
-        for(char c : line){
-            if(c != player) answer = false;
+    private static boolean checkLine(char player, char[] line) {
+        if (player == ' ') return false;
+        for (char c : line) {
+            if (c != player) return false;
         }
-        return answer;
+        return true;
     }
 
-    public static char[][] generateGameBoard(int size){
+    public static char[][] generateGameBoard(int size) {
         char[][] board = new char[size][size];
-        for (int i = 0; i < size; i++){
-            for (int j = 0; j < size; j++){
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
                 board[i][j] = ' ';
-            }
-        }
         return board;
     }
 }
